430e733bfb3f8ad27c583b0bbf814f8b1a7607f7
progden <ccdrumer@gmail.com>
2024-05-04 19:22:38 +0800

    重構測試碼並增強測試驗證
    本次提交主要對 TennisGameUi 的測試碼進行了重構，並加強了對模擬物件的驗證：
    - 重新組織了部分匯入模組的順序以符合慣例。
    - 強化了模擬物件的使用，更專注對 score 方法呼叫次數的驗證。
    - 將重複的點擊和驗證方法抽離出來，鎖定指定玩家並比對指定的點數。
    - 測試命名也從英文轉成中文，以便於理解。

--- src/components/TennisGameUi.test.ts
+++ src/components/TennisGameUi.test.ts
@@ -1,45 +1,54 @@
-import {test, expect, suite} from "vitest";
+import {expect, suite, test, vi} from "vitest";
 
 import {mount} from "@vue/test-utils";
 
 import TennisGameUI from "./TennisGameUI.vue";
-import {vi} from "vitest";
 
+// mock TennisGameUI 的倚賴物件(Domain Model)
+// 透過 spy 和 自訂的 list 來驗證是否呼叫了對應的方法
+const scoreSpy = vi.fn();
 const list: string[] = [];
 vi.mock('../domain/TennisGame/tennisGame.ts', () => ({
     TennisGame: vi.fn().mockImplementation(() => ({
-        score: vi.fn().mockReturnValue('Some Tennis Score'),
+        score: scoreSpy.mockReturnValue('Some Tennis Score'),
         wonPoint: (name: string) => list.push(name),
     }))
 }));
 
-
 const vueWrapper = mount(TennisGameUI);
 
-function click(selector: string, times: number = 1) {
+suite("TennisGameUi", () => {
+    test("當遊戲開始時，應返回模擬物件的訊息", () => {
+        const msg = vueWrapper.get(".msg");
+        expect(msg.text()).toContain("Some Tennis Score");
+        expect(scoreSpy).toHaveBeenCalledTimes(1);
+    });
+
+    test("當點擊按鈕時應正確地呼叫 wonPoint 和 score", () => {
+        givenClick("#p1Score");
+        shouldWonPointCount("player1", 1);
+        shouldeGetScoreBeCalledTimes(2);
+
+        givenClick("#p1Score", 2);
+        shouldWonPointCount("player1", 3);
+        shouldeGetScoreBeCalledTimes(4);
+
+        givenClick("#p2Score");
+        shouldWonPointCount("player2", 1);
+        shouldeGetScoreBeCalledTimes(5);
+    });
+});
+
+function givenClick(selector: string, times: number = 1) {
     for (let i = 0; i < times; i++)
         vueWrapper.get(selector).trigger("click");
 }
 
-function shouldCount(player: string, times: number) {
-    const filter = list.filter(v => v === player)
-    expect(filter).toHaveLength(times);
+function shouldWonPointCount(player: string, times: number) {
+    expect(list.filter(v => v === player))
+        .toHaveLength(times);
 }
 
-suite("TennisGameUi", () => {
-    test("should return message from mock when the game starts", () => {
-        const msg = vueWrapper.get(".msg");
-        expect(msg.text()).toContain("Some Tennis Score");
-    });
-
-    test("should call wonPoint correctly when click button", () => {
-        click("#p1Score");
-        shouldCount("player1", 1);
-        click("#p1Score", 2);
-        shouldCount("player1", 3);
-
-        click("#p2Score");
-        shouldCount("player1", 3);
-        shouldCount("player2", 1);
-    });
-});
+function shouldeGetScoreBeCalledTimes(number: number) {
+    expect(scoreSpy).toHaveBeenCalledTimes(number);
+}
